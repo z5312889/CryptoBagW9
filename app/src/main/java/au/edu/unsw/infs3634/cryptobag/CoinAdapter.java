2b479446fc4090de4e6cf2e831584c8e2ba636a5
z5312889 <79146278+z5312889@users.noreply.github.com>
2022-03-16 19:32:46 +1100

    tutorial 5

--- app/src/main/java/au/edu/unsw/infs3634/cryptobag/CoinAdapter.java
+++ app/src/main/java/au/edu/unsw/infs3634/cryptobag/CoinAdapter.java
@@ -1,77 +1,137 @@
 package au.edu.unsw.infs3634.cryptobag;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
 
 public class CoinAdapter extends RecyclerView.Adapter<CoinAdapter.CoinViewHolder> {
-    //public class CoinAdapter extends AppCompatActivity {
-    private ArrayList<Coin> mCoins;
+    public static final int SORT_COIN_NAME = 1;
+    public static final int SORT_COIN_PRICE = 2;
+    private ArrayList<Coin> mCoins, mCoinsFiltered;
     private RecyclerViewClickListener mListener;
+    // private static final int SORT_COIN_NAME = 1;
+    // private static final int SORT_COIN_PRICE = 2;
 
+    // Constructor method for CoinAdapter class
     public CoinAdapter(ArrayList<Coin> coins, RecyclerViewClickListener listener) {
         mCoins = coins;
         mListener = listener;
+        mCoinsFiltered = coins;
     }
 
+    //@Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                String charString = constraint.toString();
+                if (charString.isEmpty()) {
+                    mCoinsFiltered = mCoins;
+                } else {
+                    ArrayList<Coin> filteredList = new ArrayList<>();
+                    for (Coin coin : mCoins) {
+                        if (coin.getName().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(coin);
+                        }
+                    }
+                    mCoinsFiltered = filteredList;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = mCoinsFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                mCoinsFiltered = (ArrayList<Coin>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    // ClickListener interface
     public interface RecyclerViewClickListener {
-        void onClick(View view, String id);
+        void onClick(View view, String coinSymbol);
     }
 
+
+    // Create a ViewHolder and return it
     @NonNull
     @Override
-    public CoinViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext().inflate(R.layout.coin_list, parest, false));
+    public CoinAdapter.CoinViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_row, parent, false);
         return new CoinViewHolder(v, mListener);
     }
 
+    // Associate data with the view holder for a given position in the RecyclerView
     @Override
     public void onBindViewHolder(@NonNull CoinAdapter.CoinViewHolder holder, int position) {
         Coin coin = mCoins.get(position);
-        holder.coin.setText(coin.getName());
-        holder.value.setText(String.valueOf(coin.getPriceUsd()));
 
-        holder.change.setText(String.valueOf(coin.getPercentChange24h()) + "%");
-        holder.itemView.setTag(coin.getName());
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        holder.name.setText(coin.getName());
+        holder.value.setText(formatter.format(Double.valueOf(coin.getPriceUsd())));
+        holder.change.setText(String.valueOf(coin.getPercentChange1h()) + " %");
+        holder.itemView.setTag(coin.getSymbol());
+
     }
 
+    // Return the number of data items available for displaying
     @Override
     public int getItemCount() {
         return mCoins.size();
     }
 
-    public class CoinViewHolder extends RecyclerView.ViewHolder implements View.onClickListener {
-        public TextView coin, value, change;
-        private RecyclerViewClickListener bListener;
+    // Extend the signature of CoinViewHolder to implement a click listener
+    public static class CoinViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView name, value, change;
+        private RecyclerViewClickListener mListener;
 
+        // Constructor method for CoinViewHolder class
         public CoinViewHolder(@NonNull View itemView, RecyclerViewClickListener listener) {
             super(itemView);
-            this.bListener = listener;
+            mListener = listener;
             itemView.setOnClickListener(this);
-            coin = itemView.findViewById(R.id.tvValue);
+            name = itemView.findViewById(R.id.tvName);
             value = itemView.findViewById(R.id.tvValue);
             change = itemView.findViewById(R.id.tvChange);
         }
 
         @Override
-        public void onClick(View v) {bListener.onClick(v.(String)v.getTag());}
-
-//    @Override
-        //   protected void onCreate(Bundle savedInstanceState) {
-        //       super.onCreate(savedInstanceState);
-        //       setContentView(R.layout.activity_coin_adapter);
-        //   }
-//}
-
-//public class CoinAdapter extends RecyclerView.Adapter<CoinAdapter.CoinViewHolder> {
-
-
-
+        public void onClick(View view) {
+            mListener.onClick(view, (String) view.getTag());
+        }
     }
+
+    public void sort(final int sortMethod) {
+        if(mCoinsFiltered.size() > 0) {
+            Collections.sort(mCoinsFiltered, new Comparator<Coin>() {
+                @Override
+                public int compare(Coin o1, Coin o2) {
+                    if(sortMethod == SORT_COIN_NAME) {
+                        // sort by coin name
+                        return o2.getName().compareTo(o1.getName());
+
+                    } else if (sortMethod == SORT_COIN_PRICE){
+                        //sort by price
+                        return Double.valueOf(o2.getPriceUsd()).compareTo(Double.valueOf(o1.getPriceUsd()));
+                    }
+                    return o2.getName().compareTo(o1.getName());
+                }
+            });
+        }
+        notifyDataSetChanged();
+    }
+
+}
--- app/src/main/java/au/edu/unsw/infs3634/cryptobag/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/cryptobag/MainActivity.java
@@ -1,14 +1,94 @@
 package au.edu.unsw.infs3634.cryptobag;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+  private static final String TAG = "MainActivity";
+  private RecyclerView mRecyclerView;
+  private CoinAdapter mAdapter;
+  private RecyclerView.LayoutManager mLayoutManager;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    // Get a handle to the RecyclerView
+    mRecyclerView = findViewById(R.id.rvList);
+    mRecyclerView.setHasFixedSize(true);
+
+    // Instantiate a LinearLayoutManager
+    mLayoutManager = new LinearLayoutManager(this);
+    mRecyclerView.setLayoutManager(mLayoutManager);
+
+    // Implement ClickListener for list items
+    CoinAdapter.RecyclerViewClickListener listener = new CoinAdapter.RecyclerViewClickListener() {
+      @Override
+      public void onClick(View view, String coinSymbol) {
+        launchDetailActivity(coinSymbol);
+      }
+    };
+    // Create an adapter instance and supply the coins data to be displayed
+    mAdapter = new CoinAdapter(Coin.getCoins(), listener);
+    // Connect the adapter with the RecyclerView
+    mRecyclerView.setAdapter(mAdapter);
+
   }
+
+  @Override
+  public boolean onCreateOptionsMenu (Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.menu_main, menu);
+    SearchView searchView = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
+    searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+      @Override
+      public boolean onQueryTextSubmit(String query) {
+        mAdapter.getFilter().filter(query);
+        return false;
+      }
+
+      @Override
+      public boolean onQueryTextChange(String newText) {
+        mAdapter.getFilter().filter(newText);
+        return false;
+      }
+    });
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item){
+    switch (item.getItemId()) {
+      case R.id.sortName:
+        mAdapter.sort(CoinAdapter.SORT_COIN_NAME);
+        return true;
+      case R.id.sortValue:
+        mAdapter.sort(CoinAdapter.SORT_COIN_PRICE);
+        return true;
+      default:
+        return  super.onOptionsItemSelected(item);
+    }
+  }
+
+  // Called when user taps on a row on the RecyclerView
+  private void launchDetailActivity(String message){
+    Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+    intent.putExtra(DetailActivity.INTENT_MESSAGE, message);
+    startActivity(intent);
+  }
+
 }
